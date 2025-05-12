4c414b49e6e3046a7b0a43e4209ed2e7bd59fb78
Pushkal-godara <godarapushkal@gmail.com>
2025-05-12 13:42:16 +0530

    changing domain name

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  // baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://pressinspection.sg/api',
+  
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
